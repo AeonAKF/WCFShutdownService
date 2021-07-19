af26d1cd86b5958b01c5a301166714d7ae9522d7
Alan Richards <87658732+AeonAKF@users.noreply.github.com>
2021-07-19 21:53:30 +0100

    Add files via upload

---
+++ ABPMilkDryfeHiberSrv/ABPMilkDryfeHiberSrvLochar/bin/Release/InstallService.bat
@@ -0,0 +1,20 @@
+@ECHO off
+
+REM Requires "Run As Administrator" 
+
+set /P c=If this file has admin rights press Y, if not, press N and RunAs Administrator[Y/N]?
+if /I "%c%" EQU "Y" goto :Admin
+if /I "%c%" EQU "N" goto :NoAdmin
+
+:Admin
+
+ECHO Installing Service...
+ECHO....................
+
+	%Windir%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe "%~dp0ABPMilkDryfeHiberSrvLochar.exe"
+
+pause
+
+:NoADmin
+
+Exit
